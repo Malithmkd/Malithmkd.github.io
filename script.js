ba3debdbd6dca0e2e3403a13be52b856e0c7b50a
Malithmkd <malith.kavidu.34@gmail.com>
2024-02-12 20:46:00 +0530

    new update files

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ var typed = new Typed(".text",{
     backDelay:100,
     loop:true
 })
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxnvZh-rWebTLMrZq_g6F9kvprJDZOwpYXZ6fbNeDn-Fu-naiOj-EmOr-ehQZz44nzZRg/exec'
+
+const form = document.forms['contact-me']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
